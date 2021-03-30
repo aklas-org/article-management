36109d1fb4c5f143928a552c6d46b635a1eb7e81
maganasamuel <maganasamuel001@gmail.com>
2021-03-30 20:39:10 +0800

    Created search scope

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -10,4 +10,20 @@ class Article extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $searchColumns = [
+        'title',
+        'writer',
+    ];
+
+    public function scopeSearch($query, $search)
+    {
+        return $query->when($search, function ($query, $search) {
+            foreach ($this->searchColumns as $searchColumn) {
+                $query->orWhere($searchColumn, 'like', '%' . $search . '%');
+            }
+
+            return $query;
+        });
+    }
 }
